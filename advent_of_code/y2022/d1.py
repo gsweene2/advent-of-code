859539cae3c5ca8d15ca668f28244a83dbad69ce
Garrett Sweeney <garrett.d.sweeney@gmail.com>
2022-12-02 16:38:33 -0500

    Part 2

--- advent_of_code/y2022/d1.py
+++ advent_of_code/y2022/d1.py
@@ -8,6 +8,8 @@ SESSION = os.environ.get("ADVENT_SESSION")
 
 def find_largest_sum(sums_url) -> int:
     inputs_list = _get_file_as_list(sums_url)
+    if inputs_list[-1]:
+        inputs_list.append(b'')
     local_sum, max_sum = 0, float("-inf")
     for i in inputs_list:
         if not i:
@@ -16,6 +18,28 @@ def find_largest_sum(sums_url) -> int:
         else:
             local_sum += int(i)
     return max_sum
+
+
+def sum_n_largest_sums(sums_url, num_sums) -> int:
+    inputs_list = _get_file_as_list(sums_url)
+    if inputs_list[-1]:
+        inputs_list.append(b'')
+    local_sum, sums = 0, []
+    for i in inputs_list:
+        if not i:
+            if not sums:
+                sums = [local_sum]
+            else:
+                sums.append(local_sum)
+            local_sum = 0
+        else:
+            local_sum += int(i)
+            
+    
+    if not sums or num_sums > len(sums):
+        raise Exception("More sums requested than exist")
+    
+    return sum(sorted(sums, reverse=True)[:num_sums])
 
 
 def _get_file_as_list(url) -> List:
@@ -31,3 +55,6 @@ def _get_file_as_list(url) -> List:
 if __name__ == "__main__":
     r = find_largest_sum(URL)
     print(f"Max Sum: {r}")
+
+    r = sum_n_largest_sums(URL, 3)
+    print(f"Sum of 3 largest sums: {r}")
--- tests/y2022/d1_test.py
+++ tests/y2022/d1_test.py
@@ -1,7 +1,7 @@
 import responses
 from unittest.mock import patch
 
-from advent_of_code.y2022.d1 import find_largest_sum, _get_file_as_list
+from advent_of_code.y2022.d1 import find_largest_sum, _get_file_as_list, sum_n_largest_sums
 
 
 @patch("advent_of_code.y2022.d1._get_file_as_list")
@@ -30,6 +30,34 @@ def test__find_largest_sum(mock__get_file_as_list):
 
     # Assert
     assert 24000 == r
+
+
+@patch("advent_of_code.y2022.d1._get_file_as_list")
+def test__find_largest_sum(mock__get_file_as_list):
+    # Arrange
+    input_list = [
+        b"1000",
+        b"2000",
+        b"3000",
+        b"",
+        b"4000",
+        b"",
+        b"5000",
+        b"6000",
+        b"",
+        b"7000",
+        b"8000",
+        b"9000",
+        b"",
+        b"10000",
+    ]
+    mock__get_file_as_list.return_value = input_list
+
+    # Act
+    r = sum_n_largest_sums("https://some-fake-url.com/input", 3)
+
+    # Assert
+    assert 45000 == r
 
 
 @responses.activate
